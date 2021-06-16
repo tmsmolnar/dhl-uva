92024d166f00c8043790d378b94c5702d456dd7a
tmsmolnar <73756619+tmsmolnar@users.noreply.github.com>
2021-06-16 11:52:19 +0200

    more functions

---
+++ functions/__init__.py
@@ -0,0 +1,1 @@
+from .functions import readPDF, tokenizePDF, lemmatizeAndStem, toDataFrame
---
+++ functions/functions.py
@@ -0,0 +1,69 @@
+# [WIP]
+
+import pandas as pd
+from nltk.corpus import stopwords
+import os
+import fitz
+import string
+import nltk
+import PyPDF2
+from nltk.tokenize import word_tokenize
+from gensim.parsing.preprocessing import STOPWORDS
+from nltk.stem import WordNetLemmatizer, SnowballStemmer
+from nltk.stem.porter import stemmer
+
+
+def readPDF(folder):
+
+    articles = dict()
+
+    for root, dirs, files in os.walk(folder):
+        for file in files:
+            file_path = os.path.join(root, file)
+
+            pdf = fitz.open(file_path)
+            text = ""
+            output = []
+
+            with open(file_path, 'rb') as f:
+                reader = PyPDF2.PdfFileReader(f)
+                numberOfPages = reader.numPages
+
+                for index in range(0, numberOfPages):
+                    page = pdf[index]
+                    text += page.getText('text')
+
+            articles[file] = text
+            output.append(text)
+
+    return articles
+
+
+def lemmatizeAndStem(data):
+    return stemmer.stem(WordNetLemmatizer().lemmatize(data, pos='v'))
+
+
+def tokenizePDF(dictionary):
+
+    tokenizedPDF = dict()
+    nltkStopWords = stopwords.words('english')
+    gensimStopWords = STOPWORDS
+
+    for keys in dictionary:
+        dictionary[keys] = dictionary[keys].replace('\n', ' ')
+
+        tokenized = word_tokenize(dictionary[keys])
+        tokenized = [word for word in tokenized if not word in nltkStopWords]
+        tokenized = [word for word in tokenized if not word in gensimStopWords]
+        tokenized = [
+            word for word in tokenized if not word in string.punctuation]
+        tokenizedPDF[keys] = lemmatizeAndStem(tokenized)
+
+    return tokenizedPDF
+
+
+def toDataFrame(data):
+
+    dataFrame = pd.DataFrame(data.items(), columns=['file_name', 'content'])
+
+    return dataFrame
